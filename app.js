51200eb7cca1750d9fc86704e02047b871c5e0fb
Mario Serrano <maseflores79@gmail.com>
2018-04-08 20:43:34 -0700

    agregado archivo notes.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-console.log('Starting app');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
+var result = notes.add(5,9);
+console.log(result);
 
+//var user = os.userInfo();
 //console.log(user);
-fs.appendFileSync('greetings.txt', `\nHello ${user.username}`);
+//fs.appendFileSync('greetings.txt', `\nHello ${user.username}, you are ${notes.age}`);
