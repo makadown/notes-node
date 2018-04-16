72d380973cd7005493b6b6d1a388c8a91e91f343
Mario Serrano <maseflores79@gmail.com>
2018-04-16 14:38:24 -0700

    agregado lodash

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var result = notes.add(5,9);
-console.log(result);
+//console.log(_.isString(true));
+//console.log(_.isString('Mario'));
+//var result = notes.add(5,9);
+//console.log(result);
+
+var filteredArray = _.uniq(['Mario', 1, 'Petra',2 ,2, 'Mario','Mario','Estela','Janine','Lupe', 1, 2 , 3, 4]);
+console.log(filteredArray);
 
 //var user = os.userInfo();
 //console.log(user);
