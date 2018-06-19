dc40486e8a02e5e7368c881c67edab34a1207a74
Mario Serrano <maseflores79@gmail.com>
2018-06-19 15:49:43 -0700

    Mas pininos con debugging y mostrando todas las notas

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -8,9 +6,6 @@ const notes = require('./notes.js');
 
 const argv = yargs.argv;
 var command = argv._[0];
-
-console.log('Command: ', command);
-console.log('Yargs', argv);
 
 if (command === 'add') {
     var note = notes.addNote(argv.title, argv.body);
@@ -21,7 +16,12 @@ if (command === 'add') {
         console.log('Note title taken');
     }
 } else if (command === 'list') {
-    notes.getAll();
+    var allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s).`);
+    allNotes.forEach(note => {
+        console.log(note);
+    });
+
 } else if (command === 'read') {
     var noteRead = notes.getNote(argv.title);
     if (noteRead) {
--- playground/debugging.js
+++ playground/debugging.js
@@ -2,7 +2,44 @@ var person = {
     name: 'Andrew'
 };
 
-person.age = 25;
+person.age = 38;
 person.name = 'Mike';
 
 console.log(person);
+
+/*
+Node debug
+
+> node inspect *.js
+
+continuar
+> c
+
+siguiente
+>  n  
+
+para ver valores hasta ese momento
+> repl
+
+como un breakpoint, en el codigo puedo poner la instruccion
+debugger;
+
+puedo correr tmb
+> nodemon inspect app.js read --title="to buy"
+
+de modo que puedo modificar mi codigo y este se reinicia y puedo debuguear agusto
+
+
+Ahora bien, si corro 
+
+> node --inspect-brk app.js read --title="to buy"
+o
+> nodemon --inspect-brk app.js read --title="to buy"
+
+Abre algo como esto:
+Debugger listening on ws://127.0.0.1:9229/df7634a6-2839-4d5a-8004-8254a8538ed5
+For help, see: https://nodejs.org/en/docs/inspector
+
+y le doy en chrome:
+chrome://inspect/    > Open dedicated devtools for Node
+*/
